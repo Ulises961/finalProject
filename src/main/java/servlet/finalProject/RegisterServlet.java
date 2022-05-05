170d5c619d603f9717ecc6003f2be22d4a32b822
Sosa Ulises Emiliano <ulisesemiliano.sosa@eurac.edu>
2022-05-05 08:34:55 +0200

    Add RSA generation and keys distribution

--- src/main/java/servlet/finalProject/RegisterServlet.java
+++ src/main/java/servlet/finalProject/RegisterServlet.java
@@ -1,103 +1,108 @@
 package servlet.finalProject;
 
-import jakarta.servlet.http.HttpServlet;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Properties;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import org.mindrot.jbcrypt.BCrypt;
+import utils.RSA;
+import utils.RSAKeys;
 
+import java.io.IOException;
+import java.math.BigInteger;
+import java.sql.*;
+import java.util.Properties;
 
 /**
  * Servlet implementation class RegisterServlet
  */
 @WebServlet("/RegisterServlet")
 public class RegisterServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
 
-	private static final String USER = "postgres";
-	private static final String PWD = "postgres";
-	private static final String DRIVER_CLASS = "org.postgresql.Driver";
-	private static final String DB_URL = "jdbc:postgresql://localhost/finalproject";
-	private static final int BCryptWorkload = 15;
+    private static final String USER = "postgres";
+    private static final String PWD = "postgres";
+    private static final String DRIVER_CLASS = "org.postgresql.Driver";
+    private static final String DB_URL = "jdbc:postgresql://localhost/finalproject";
+    private static final int BCryptWorkload = 15;
 
-	private static Connection conn;
+    private static Connection conn;
 
 
-	/**
+    /**
      * @see HttpServlet#HttpServlet()
      */
     public RegisterServlet() {
         super();
     }
-    
+
     public void init() throws ServletException {
-    	try {
-			Class.forName(DRIVER_CLASS);
-			
-		    Properties connectionProps = new Properties();
-		    connectionProps.put("user", USER);
-		    connectionProps.put("password", PWD);
-	
-	        conn = DriverManager.getConnection(DB_URL, connectionProps);
-		    
-		    //System.out.println("User \"" + USER + "\" connected to database.");
-    	
-    	} catch (ClassNotFoundException | SQLException e) {
-			e.printStackTrace();
-		}
+        try {
+            Class.forName(DRIVER_CLASS);
+
+            Properties connectionProps = new Properties();
+            connectionProps.put("user", USER);
+            connectionProps.put("password", PWD);
+
+            conn = DriverManager.getConnection(DB_URL, connectionProps);
+
+            //System.out.println("User \"" + USER + "\" connected to database.");
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-		
-		// The replacement escapes apostrophe special character in order to store it in SQL
-		String name = request.getParameter("name");
-		String surname = request.getParameter("surname");
-		String email = request.getParameter("email");
-		String pwd = request.getParameter("password");
-		
-		String salt = BCrypt.gensalt(BCryptWorkload);
-	    String pwdhash = BCrypt.hashpw(pwd, salt);
-		
-		try (Statement st = conn.createStatement()) {
-			ResultSet sqlRes = st.executeQuery(
-				"SELECT * "
-				+ "FROM users "
-				+ "WHERE email='" + email + "'"
-			);
-			
-			if (sqlRes.next()) {
-				System.out.println("Email already registered!");
-				request.getRequestDispatcher("register.html").forward(request, response);
-				
-			} else {
-				st.execute(
-					"INSERT INTO users ( name, surname, email, password ) "
-					+ "VALUES ( '" + name + "', '" + surname + "', '" + email + "', '" + pwdhash + "' )"
-				);
-				
-				request.setAttribute("email", email);
-				request.setAttribute("password", pwdhash);
-				
-				System.out.println("Registration succeeded!");
-				request.getRequestDispatcher("home.jsp").forward(request, response);
-			}
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-			request.getRequestDispatcher("register.html").forward(request, response);
-		}
-	}
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        // The replacement escapes apostrophe special character in order to store it in SQL
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        String email = request.getParameter("email");
+        String pwd = request.getParameter("password");
+
+        String salt = BCrypt.gensalt(BCryptWorkload);
+        String pwdhash = BCrypt.hashpw(pwd, salt);
+        RSA rsa = new RSA();
+        RSAKeys keys = rsa.generateKeys();
+        BigInteger privKey = keys.getE();
+        BigInteger pubKey = keys.getD();
+
+        try (Statement st = conn.createStatement()) {
+            ResultSet sqlRes = st.executeQuery(
+                    "SELECT * "
+                            + "FROM users "
+                            + "WHERE email='" + email + "'"
+            );
+
+            if (sqlRes.next()) {
+                System.out.println("Email already registered!");
+                request.getRequestDispatcher("register.html").forward(request, response);
+
+            } else {
+                st.execute(
+                        "INSERT INTO users ( name, surname, email, password ) "
+                                + "VALUES ( '" + name + "', '" + surname + "', '" + email + "', '" + pwdhash + "' ); " +
+                                "INSERT INTO public_keys(pub,utente) VALUES (" + pubKey + ",'" + email + "')"
+                );
+
+                request.setAttribute("email", email);
+                request.setAttribute("password", pwdhash);
+
+                request.setAttribute("privKey", privKey);
+                System.out.println("Registration succeeded!");
+                System.out.println("User private key " + privKey);
+                System.out.println("User public key " + pubKey);
+                request.getRequestDispatcher("home.jsp").forward(request, response);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            request.getRequestDispatcher("register.html").forward(request, response);
+        }
+    }
 
 }
--- src/main/webapp/home.jsp
+++ src/main/webapp/home.jsp
@@ -1,36 +1,54 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1" %>
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="ISO-8859-1">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="style.css" type="text/css" />
-	
-	<title>Home page</title>
+    <meta charset="ISO-8859-1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css" type="text/css"/>
+
+    <title>Home page</title>
 </head>
 <body>
-	<nav class="navbar">
-	  <div class="box">
-	  	<div>
-			<img src="images/email_icon.jpg" align="left" />
-			<p>E-MAIL CLIENT
-				<br><% out.println(request.getAttribute("email")); %>
-			</p>
-	  	</div>
-	  	<div id="right"><a href="login.html">Logout</a></div>
-	  </div>
-	</nav>
-	
-	<div class="grid-container">
-		<form class="btn-group" action="NavigationServlet" method="post">
-			<input type="hidden" name="email" value="<%= request.getAttribute("email") %>">
-			<input type="hidden" name="password" value="<%= request.getAttribute("password") %>">
-			<input type="submit" name="newMail" value="New Mail">
-			<input type="submit" name="inbox" value="Inbox">
-			<input type="submit" name="sent" value="Sent">
-		</form>
-		
-		<%= request.getAttribute("content")!=null ? request.getAttribute("content") : "" %>
-	</div>
+<nav class="navbar">
+    <div class="box">
+        <div>
+            <img src="images/email_icon.jpg" align="left"/>
+            <p>E-MAIL CLIENT
+                <br><% out.println(request.getAttribute("email")); %>
+            </p>
+        </div>
+        <div id="right"><a href="login.html">Logout</a></div>
+    </div>
+</nav>
+
+<div class="grid-container">
+    <form class="btn-group" action="NavigationServlet" method="post">
+        <input type="hidden" name="email" value="<%= request.getAttribute("email") %>">
+        <input type="hidden" name="password" value="<%= request.getAttribute("password") %>">
+        <input type="submit" name="newMail" value="New Mail">
+        <input type="submit" name="inbox" value="Inbox">
+        <input type="submit" name="sent" value="Sent">
+    </form>
+
+    <%= request.getAttribute("content") != null ? request.getAttribute("content") : "" %>
+</div>
+
+<div class="modal-content">
+    <div class="modal-header">
+        <span class="close">&times;</span>
+        <h2>Modal Header</h2>
+    </div>
+    <div class="modal-body">
+        <% if (request.getAttribute("privKey") != null) { %>
+        <p><% request.getAttribute("privKey"); %></p>
+        <script>alert(<% request.getAttribute("privKey"); %>);
+        console.log(<% request.getAttribute("privKey"); %>)</script>
+        <% } %>
+    </div>
+    <div class="modal-footer">
+        <h3>Modal Footer</h3>
+    </div>
+</div>
 </body>
 </html>
+
