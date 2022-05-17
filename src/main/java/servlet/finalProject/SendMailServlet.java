83ffa6ae1726fed0edee599b19c4b0ab0615b41e
Balawal Sultan <balawalsultan786@gmail.com>
2022-05-17 14:40:57 +0200

    use prepared statemets in SendMailServlet
    Note that the time column in the database has been changed to timestamp.

--- src/main/java/servlet/finalProject/SendMailServlet.java
+++ src/main/java/servlet/finalProject/SendMailServlet.java
@@ -1,19 +1,25 @@
 package servlet.finalProject;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Properties;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import utils.RSA;
-
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.sql.*;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Properties;
 
 /**
  * Servlet implementation class SendMailServlet
@@ -65,24 +71,27 @@ public class SendMailServlet extends HttpServlet {
         String subject = request.getParameter("subject");
         String body = request.getParameter("body");
         String time = LDT_FORMATTER.format(LocalTime.now());
+        
+        String encryptedBody = encryptMailBody(body, receiver);
 
-        // System.out.println(getPublicKey(receiver)[0].intValue());
-        BigInteger[] keys = getPublicKey(receiver);
-        RSA rsa = new RSA();
-
-        BigInteger[] encryptedBody = rsa.encrypt(body, new BigInteger("3"), new BigInteger("391"));
-        String newBody = "" + encryptedBody[0];
-
-        for (int i = 1; i < encryptedBody.length; i++)
-            newBody += "," + encryptedBody[i];
-
-        try (Statement st = conn.createStatement()) {
-            st.execute(
-                    "INSERT INTO mail ( sender, receiver, subject, body, time[2] ) "
-                            + "VALUES ( '" + sender + "', '" + receiver + "', '" + subject + "', '" + newBody + "', '"
-                            + time + "' )");
+        try{
+        	String sendMail = "INSERT INTO mail ( sender, receiver, subject, body, time ) VALUES (?, ?, ?, ?, ?);";
+        	PreparedStatement pstm = conn.prepareStatement(sendMail);
+        	pstm.setString(1, sender);
+        	pstm.setString(2, receiver);
+        	pstm.setString(3, subject);
+        	pstm.setString(4, encryptedBody);
+        	pstm.setTimestamp(5, Timestamp.valueOf(LocalDateTime.now()));
+        	
+        	int numUpdates = pstm.executeUpdate();
+        	
+        	if(numUpdates == 1)
+        		System.out.println("Email succesfully sent!");
+        	else
+        		System.out.println("Could not send email!");
 
         } catch (SQLException e) {
+        	System.out.println("Could not send email!");
             e.printStackTrace();
         }
 
@@ -92,19 +101,35 @@ public class SendMailServlet extends HttpServlet {
 
     //
 
+    protected String encryptMailBody(String body, String receiver) {
+    	 BigInteger[] keys = getPublicKey(receiver);
+         RSA rsa = new RSA();
+
+         BigInteger[] encryptedBody = rsa.encrypt(body, new BigInteger("3"), new BigInteger("391"));
+         String newBody = "" + encryptedBody[0];
+
+         for (int i = 1; i < encryptedBody.length; i++)
+             newBody += "," + encryptedBody[i];
+         
+         return newBody;
+    }
+    
     protected BigInteger[] getPublicKey(String receiver) {
         BigInteger[] keys = new BigInteger[2];
 
-        try (Statement st = conn.createStatement()) {
-            ResultSet rs = st.executeQuery(
-                    "SELECT pub,n  FROM public_keys WHERE utente ='" + receiver + "'");
+        try{
+        	String sendMail = "SELECT pub,n  FROM public_keys WHERE utente =?;";
+        	PreparedStatement pstm = conn.prepareStatement(sendMail);
+        	pstm.setString(1, receiver);
+        	
+            ResultSet result = pstm.executeQuery();
 
-            while (rs.next()) {
-                BigDecimal decimalPub = rs.getBigDecimal("pub");
+            while (result.next()) {
+                BigDecimal decimalPub = result.getBigDecimal("pub");
                 BigInteger pub = decimalPub.toBigInteger();
                 keys[0] = pub;
 
-                BigDecimal decimalN = rs.getBigDecimal("n");
+                BigDecimal decimalN = result.getBigDecimal("n");
                 BigInteger n = decimalN.toBigInteger();
                 keys[1] = n;
 
@@ -119,7 +144,6 @@ public class SendMailServlet extends HttpServlet {
         System.out.println("An error occurred while getting the pub key");
 
         return keys;
-
     }
 
 }
